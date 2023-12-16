f3a2867db7c8cf9d241d4cdaa183c16238b8481a
Lenovo X1 <maataouihamza53@gmail.com>
2023-12-16 13:48:22 +0100

    first commit

---
+++ service-voiture/src/main/java/com/example/servicevoiture/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.example.servicevoiture.repository;
+
+import com.example.servicevoiture.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Long> {
+}
